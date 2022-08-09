8f2ac2d58c6d7e151745937367fca4c92de9055a
iLouis88 <vthanhf68@gmail.com>
2022-08-10 00:44:32 +0700

    Update file

---
+++ lib/testdart.dart
@@ -0,0 +1,19 @@
+
+void main() {
+  double a = 10, b = -50, c = 11, d = 6, e = 8, f = -5, g = 15;
+
+  List <double> numbers = [a,b,c,d,e,f,g];
+
+  if (numbers.isNotEmpty) {
+
+    numbers.sort((a,b) => b.compareTo(a));// Sap xep tu lon den be - nguoc chieu
+
+    double max  = numbers.first;
+    print (max);
+  }
+
+  // double max = a;
+  // if (max < b) max = b;
+  // if (max <c ) max = c;
+  // print (max);
+}
